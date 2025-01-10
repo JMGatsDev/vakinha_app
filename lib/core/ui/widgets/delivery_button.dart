75bbd0cc160a72e43ffc2bd2c7a4fed793372284
João Marcos <joaogatis.hrzon@gmail.com>
2025-01-10 14:40:09 -0300

    feat: editing button

--- lib/core/ui/widgets/delivery_button.dart
+++ lib/core/ui/widgets/delivery_button.dart
@@ -1,27 +1,30 @@
 import 'package:flutter/material.dart';
-
-import '../styles/constants.dart';
+import 'package:vakinha_app/core/ui/styles/constants.dart';
 
 class DeliveryButton extends StatelessWidget {
   const DeliveryButton(
       {super.key,
-      required this.text,
-      this.onPressed,
-       this.width,
-       this.height});
+      required this.onPressed,
+      required this.label,
+      this.width,
+      this.height = 50});
 
-  final String text;
   final VoidCallback? onPressed;
+  final String label;
   final double? width;
   final double? height;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text(
-        text,
-        style: MyTextStyles.textButtonLabel,
+    return SizedBox(
+      width: width,
+      height: height,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(
+          label,
+          style: MyTextStyles.textButtonLabel,
+        ),
       ),
     );
   }
